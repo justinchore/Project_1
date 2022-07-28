6839849066a3256676a2a101fc6109269aa7059a
justinchore <jchoustin91@gmail.com>
2022-07-27 20:32:07 -0700

    Orders SQL

--- query_sketches/users_account_views.sql
+++ query_sketches/users_account_views.sql
@@ -7,4 +7,12 @@
             VALUES(1, "Justin", "Cho", "justin@justin.com", "Password123!", False, "12345 Somewhere st. CA, USA 12345");
         -- The user is logged in -> directed to the main menu (genre, search)
     -- if login:
-            
+            -- ask for email, then password
+            -- search for email first. If email doesn't exist in the database(null), direct user back to the main menu
+            SELECT * FROM Users WHERE email_address = "justin@justin.com";
+            -- if user exists, check password
+                -- compare data.password with userinput
+                -- if successful, direct to books menu. If failed, take user back to input. 
+    
+-- User's Order
+    -- 
